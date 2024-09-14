d1cf117604b8a1b3c11c35367a0e17db69990c7d
sanjaikumarsankar <sanjai.sankar@essent.nl>
2024-09-14 08:59:18 +0200

    updated gitignore

--- .history/apps/cockpit/src/app/app.routes_20240914083836.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [{
-    path: '',
-    loadComponent: () => 
-        import('@angular-monorepo/contracting').then(m) => m.ContractingComponent
-},];
